37700b8ddb0c430dbd27d1c71acdf6836190933e
Harshad Kulkarni <harshkonline@gmail.com>
2016-02-15 14:41:27 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 @app.route('/portfolio')
 def portfolio():
